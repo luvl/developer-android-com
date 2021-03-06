340c65c2bff55f55ec2ff2492699b9b75041cf0f
luvl <linhnguyen.luvl@gmail.com>
2021-03-07 01:41:44 +0700

    update expresso testing

---
+++ get-started/TwoActivities/app/src/androidTest/java/com/example/twoactivities/ActivityInputOutputTest.java
@@ -0,0 +1,58 @@
+package com.example.twoactivities;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActivityInputOutputTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    // test whether the SecondActivity View elements appear when clicking the Button and vice versa
+    @Test
+    public void activityLaunch() {
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_second)).perform(click());
+        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+    }
+
+    // test for text input MainActivity and output SecondActivity
+    @Test
+    public void textInputOutput() {
+        onView(withId(R.id.editText_main)).perform(typeText("This is a test."));
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_message)).check(matches(withText("This is a failing test.")));
+    }
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.twoactivities", appContext.getPackageName());
+    }
+}
